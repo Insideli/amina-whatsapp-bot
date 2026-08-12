87b744c92ebae7b6c859df167bb6bc0d6f78a90f
Insideli <assakill799@gmail.com>
2026-08-13 02:21:46 +0500

    Initial WhatsApp webhook

---
+++ api/webhook.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+      return res.status(200).send(challenge);
+    }
+
+    return res.sendStatus(403);
+  }
+
+  if (req.method === "POST") {
+    console.log("WhatsApp webhook:", JSON.stringify(req.body, null, 2));
+
+    return res.sendStatus(200);
+  }
+
+  return res.sendStatus(405);
+}
